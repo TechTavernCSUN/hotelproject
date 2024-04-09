ef1d99b994effdf260aaa4544f17c147c3d63a1e
Mikeantabian <mikeantabian@gmail.com>
2024-04-09 16:17:48 -0700

    removed extra code

--- HotelSystem/src/DatabaseManager.java
+++ HotelSystem/src/DatabaseManager.java
@@ -15,17 +15,6 @@ public class DatabaseManager {
         }
     }
 
-    // Methods for CRUD operations (Create, Read, Update, Delete)
-
-    public List<Room> getAvailableRooms(Date checkInDate, Date checkOutDate, RoomType roomType) {
-        // Implement logic to query database for available rooms within the given date range and room type
-        // Return a list of available rooms
-        // Example SQL query: SELECT * FROM Rooms WHERE available = true AND type = 'SINGLE';
-        // You need to perform JOINs and other necessary operations based on your database schema
-        // Make sure to handle database exceptions properly
-        // Use PreparedStatement to avoid SQL injection
-    }
-
     public void addReservation(Reservation reservation) {
         // Implement logic to add a reservation to the database
         // Example SQL query: INSERT INTO Reservations (client_id, room_id, check_in_date, check_out_date) VALUES (?, ?, ?, ?);
@@ -39,50 +28,4 @@ public class DatabaseManager {
         // Make sure to handle database exceptions properly
         // Use PreparedStatement to avoid SQL injection
     }
-
-    public List<Reservation> getAllReservations() {
-        // Implement logic to retrieve all reservations from the database
-        // Example SQL query: SELECT * FROM Reservations;
-        // Make sure to handle database exceptions properly
-        // Use PreparedStatement to avoid SQL injection
-    }
-
-    public Reservation getReservationById(int reservationId) {
-        // Implement logic to retrieve a reservation by its ID from the database
-        // Example SQL query: SELECT * FROM Reservations WHERE id = ?;
-        // Make sure to handle database exceptions properly
-        // Use PreparedStatement to avoid SQL injection
-    }
-
-    public List<Reservation> getReservationsByClient(Client client) {
-        // Implement logic to retrieve all reservations of a client from the database
-        // Example SQL query: SELECT * FROM Reservations WHERE client_id = ?;
-        // Make sure to handle database exceptions properly
-        // Use PreparedStatement to avoid SQL injection
-    }
-
-    public void addClient(Client client) {
-        // Implement logic to add a new client to the database
-    }
-
-    public void updateClient(Client client) {
-        // Implement logic to update client information in the database
-    }
-
-    public void deleteClient(Client client) {
-        // Implement logic to delete a client and associated reservations from the database
-    }
-
-    // public void addHotel(Hotel hotel) {
-    //     // Implement logic to add a new hotel to the database
-    // }
-
-    // public void updateHotel(Hotel hotel) {
-    //     // Implement logic to update hotel information in the database
-    // }
-
-    // public void deleteHotel(Hotel hotel) {
-    //     // Implement logic to delete a hotel, associated rooms, and reservations from the database
-    // }
-
 }
--- HotelSystem/src/PaymentProcessor.java
+++
@@ -1,9 +0,0 @@
-
-
-public class PaymentProcessor {
-    public boolean processPayment(Client client, double amount) {
-        // Implement logic to process payment for a reservation
-        // You can integrate with a payment gateway or use mock payment processing
-        // Return true if payment is successful, false otherwise
-    }
-}
