fa2977d8732756bce4030e4e37cd1a456b98edf6
Mikeantabian <mikeantabian@gmail.com>
2024-04-30 10:45:54 -0700

    manager login now changes the primary page and requires login to use manager buttons

--- demoProj/src/main/java/com/mycompany/demoproj/ManagerLoginController.java
+++ demoProj/src/main/java/com/mycompany/demoproj/ManagerLoginController.java
@@ -4,18 +4,22 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 
 public class ManagerLoginController {
 
-    // Inject the PrimaryController instance if needed
-    // @FXML
-    // private PrimaryController primaryController;
+    @FXML
+    private PrimaryController primaryController;
+    
+    @FXML
+    private BorderPane managerLoginRoot;
 
     // Define a map to store email and password pairs for manager accounts
-    private Map<String, String> managerAccounts;
+    private final Map<String, String> managerAccounts;
 
     // Inject the email and password fields from the FXML
     @FXML
@@ -27,6 +31,10 @@ public class ManagerLoginController {
     @FXML
     private Label errorLabel;
 
+    
+    public void setPrimaryController(PrimaryController primaryController) {
+        this.primaryController = primaryController;
+    }
 
     // Initialize the map with the predefined accounts
     public ManagerLoginController() {
@@ -50,6 +58,10 @@ public class ManagerLoginController {
             if (managerAccounts.get(email).equals(password)) {
                 // If both email and password match, proceed with sign in
                 // Handle successful sign-in here
+                Scene scene = managerLoginRoot.getScene();
+                PrimaryController primaryController = (PrimaryController) scene.getUserData();
+                
+                primaryController.setManagerLoggedIn(true);
                 System.out.println("Manager signed in successfully");
                 App.setRoot("primary");
             } else {
